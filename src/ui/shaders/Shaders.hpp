3a258b27c1be1633486a7a518ddf2d2f1756f8d2
exuvo <exuvo@exuvo.se>
2021-01-05 22:58:15 +0100

    add old shaders

---
+++ src/ui/shaders/Shaders.hpp
@@ -0,0 +1,16 @@
+/*
+ * Shaders.hpp
+ *
+ *  Created on: Jan 5, 2021
+ *      Author: exuvo
+ */
+
+#ifndef SRC_UI_SHADERS_SHADERS_HPP_
+#define SRC_UI_SHADERS_SHADERS_HPP_
+
+class Shaders {
+	static void init();
+};
+
+#endif /* SRC_UI_SHADERS_SHADERS_HPP_ */
+
