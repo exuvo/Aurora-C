240e522c8bb626a2c1f843ff92b4bb878d2c1af2
exuvo <exuvo@exuvo.se>
2021-01-05 23:00:14 +0100

    Add stub shader class

--- src/ui/shaders/Shaders.hpp
+++ src/ui/shaders/Shaders.hpp
@@ -8,8 +8,15 @@
 #ifndef SRC_UI_SHADERS_SHADERS_HPP_
 #define SRC_UI_SHADERS_SHADERS_HPP_
 
+//#include "CircleShader.hpp"
+//#include "DiskShader.hpp"
+//#include "PointShader.hpp"
+
 class Shaders {
 	static void init();
+//	static inline CircleShader circle;
+//	static inline DiskShader disk;
+//	static inline PointShader point;
 };
 
 #endif /* SRC_UI_SHADERS_SHADERS_HPP_ */
